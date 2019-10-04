4145f3eb5eb617fa77ab0e76ce7ad401f644d9b4
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-04 14:06:23 +0200

    inicio

---
+++ hola/mundo/app/HolaMundoApp.java
@@ -0,0 +1,20 @@
+/** 
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo.app;
+
+/**
+ * The HolaMundoApp class implements an application that
+ * simply prints "Hola Mundo!" to standard output.
+ */
+public class HolaMundoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo!");
+    }
+    
+}
