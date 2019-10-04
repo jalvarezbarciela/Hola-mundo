1b20fb4a7084c4d085920965d5dac7e2faa3d93d
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-04 14:29:32 +0200

    version2

--- hola/mundo/app/HolaMundoApp.java
+++ hola/mundo/app/HolaMundoApp.java
@@ -14,7 +14,7 @@ public class HolaMundoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hola Mundo!");
+       System.out.println("Hola Mundo 2!");
     }
     
 }
